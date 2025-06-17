e90cb72660ec7ed5cf7e36b5e1ecb8d4aa507fb9
Dimitri Lopez <dimitrilopez01@gmail.com>
2025-06-16 22:41:12 -0400

    Adding extensions to Brave

--- brave.nix
+++ brave.nix
@@ -1,9 +1,27 @@
 { config, lib, pkgs, ... }:
-
+# https://stackoverflow.com/questions/8946325/chrome-extension-id-how-to-find-it
 {
-  # Module installing brave as default browser
-  home.packages = [ pkgs.brave ];
 
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+
+    # https://www.reddit.com/r/NixOS/comments/1bqilmi/how_to_configure_brave_browser_package_to_install/
+    # Look at the url for the id either on the chrome web store or within the brave extensions page
+    extensions = [
+      # https://chromewebstore.google.com/detail/bitwarden-password-manage/nngceckbapebfimnlniiiahkandclblb
+      { id = "nngceckbapebfimnlniiiahkandclblb"; }
+      # https://chromewebstore.google.com/detail/grammarly-ai-writing-and/kbfnbcaeplbcioakkpcpgfkobkghlhen
+      { id = "kbfnbcaeplbcioakkpcpgfkobkghlhen"; }
+      # https://chromewebstore.google.com/detail/vimium-c-all-by-keyboard/hfjbmagddngcpeloejdejnfgbamkjaeg
+      { id = "hfjbmagddngcpeloejdejnfgbamkjaeg";}
+      # https://chromewebstore.google.com/detail/youtube-ad-accelerator-ea/lmcggcabhocpfkbddekmconplfjmmgmn
+      { id = "lmcggcabhocpfkbddekmconplfjmmgmn";}
+    ];
+    commandLineArgs = [
+      "--disable-features=WebRtcAllowInputVolumeAdjustment"
+    ];
+  };
   xdg.mimeApps.defaultApplications = {
   "text/html" = "brave-browser.desktop";
   "x-scheme-handler/http" = "brave-browser.desktop";
