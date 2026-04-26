e9362a082cc2190775320cf836d915ae8b7d32df
Dimitri <dimitrilopez01@gmail.com>
2026-04-26 16:21:34 -0400

    feat(nix): add dimitril to trusted-users
    Allows user to accept flake substituters and trusted-public-keys
    without root, enabling Noctalia Cachix cache on rebuild.

--- configuration.nix
+++ configuration.nix
@@ -174,4 +174,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "dimitril" ];
 }
